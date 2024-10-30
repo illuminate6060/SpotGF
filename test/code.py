7b67537d1522a78586f3c492f5d24adbdb667ca7
illuminate6060 <50705363+illuminate6060@users.noreply.github.com>
2024-10-30 22:12:34 +0800

    Add files via upload

---
+++ test/code.py
@@ -0,0 +1,22 @@
+import SpotGF
+
+gem_path = 'path/SpotGF/test/demo.gem'  #please change this file path base on your work directory
+output =  'path/SpotGF_test/SpotGF/test'  #please change this output path base on your work directory
+
+gem_path = '/data/public/dulin/python_code/18.GFcode/SpotGF_test/SpotGF/test/demo.gem'  #please change this file path base on your work directory
+output =  '/data/public/dulin/python_code/18.GFcode/SpotGF_test/SpotGF/test/'  #please change this output path base on your work directory
+
+
+binsize =75
+lower= 0
+upper = 100000
+proportion  = 0.1
+max_iterations = 10000
+auto_threshold = True
+visualize=True
+spot_size=20
+
+
+spotgf =SpotGF.SpotGF(gem_path,binsize,proportion,auto_threshold,lower,upper,max_iterations,output,visualize,spot_size)
+GF_df = spotgf.calculate_GFscore(gem_path,binsize)
+new_gem  = spotgf.generate_GFgem(gem_path,GF_df,proportion,auto_threshold,visualize,spot_size)
